c9aeedee81368f6e2e7e32eefb6acdd4a6397c7b
IslamTashiev <ueaten04@gmail.com>
2021-06-15 23:06:10 +0600

    add new files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+import Hero from "./Store/components/Hero/Hero";
+import NavBar from "./Store/components/NavBar/NavBar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
